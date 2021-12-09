0f02efb70efb3829225ca761acfcbe97be5e704c
Gamzat <unichtojenmusor@gmail.com>
2021-12-09 18:22:52 +0300

    chore: remove redundant comments

--- src/JavaBigDecimal.java
+++ src/JavaBigDecimal.java
@@ -7,30 +7,6 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 
 
-/**
- * in
- * 9
- * -100
- * 50
- * 0
- * 56.6
- * 90
- * 0.12
- * .12
- * 02.34
- * 000.000
- * out
- * 90
- * 56.6
- * 50
- * 02.34
- * 0.12
- * .12
- * 0
- * 000.000
- * -100
- */
-
 public class JavaBigDecimal {
     public static void main(String[] args) {
         //Input
