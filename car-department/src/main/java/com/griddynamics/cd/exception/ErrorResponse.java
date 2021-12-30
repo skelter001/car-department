dbf87a1ae018b446ea414a7be8ad5d60d903bc10
Gamzat <unichtojenmusor@gmail.com>
2021-12-30 16:28:33 +0300

    feat: Add ExceptionAdviser

--- car-department/src/main/java/com/griddynamics/cd/exception/ErrorResponse.java
+++ car-department/src/main/java/com/griddynamics/cd/exception/ErrorResponse.java
@@ -1,19 +1,23 @@
 package com.griddynamics.cd.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
+    private final LocalDateTime timestamp;
     private final int status;
     private final String message;
     private List<ValidationError> errors;
@@ -21,7 +25,7 @@ public class ErrorResponse {
     @Getter
     @Setter
     @RequiredArgsConstructor
-    private static class ValidationError {
+    public static class ValidationError {
         private final String field;
         private final String message;
     }
--- car-department/src/main/java/com/griddynamics/cd/exception/ExceptionAdviser.java
+++ car-department/src/main/java/com/griddynamics/cd/exception/ExceptionAdviser.java
@@ -1,26 +1,60 @@
 package com.griddynamics.cd.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionAdviser extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException enfe) {
-        return buildErrorResponse(enfe, HttpStatus.NOT_FOUND);
+        log.error("Failed to find expected entity", enfe);
+        return buildErrorResponse(enfe.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now(), null);
     }
 
-    private ResponseEntity<Object> buildErrorResponse(Exception ex,
-                                                      HttpStatus httpStatus) {
-        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), ex.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException manve, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        List<ErrorResponse.ValidationError> validationErrors = manve.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(val -> new ErrorResponse.ValidationError(val.getField(), val.getDefaultMessage()))
+                .toList();
+
+        String[] msg = manve.getMessage().split("\\s+");
+
+        return buildErrorResponse(msg[0] + ' ' + msg[1], HttpStatus.BAD_REQUEST, LocalDateTime.now(), validationErrors);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    public ResponseEntity<Object> handleTooManyRequestException(HttpClientErrorException hcee) {
+        log.error("Too many requests exception was caused", hcee);
+        return buildErrorResponse(hcee.getMessage(), HttpStatus.TOO_MANY_REQUESTS, LocalDateTime.now(), null);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(String msg,
+                                                      HttpStatus httpStatus,
+                                                      LocalDateTime timestamp,
+                                                      List<ErrorResponse.ValidationError> validationErrors) {
+        ErrorResponse errorResponse = new ErrorResponse(timestamp, httpStatus.value(), msg, validationErrors);
         return ResponseEntity.status(httpStatus).body(errorResponse);
     }
 }
