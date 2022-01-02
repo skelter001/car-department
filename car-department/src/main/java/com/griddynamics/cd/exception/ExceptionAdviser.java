4506479e2fdeb15b25e460b3dc39592fc060bcc2
Gamzat <unichtojenmusor@gmail.com>
2022-01-02 15:09:41 +0300

    feat: Add handling of json parse error

--- car-department/src/main/java/com/griddynamics/cd/exception/ExceptionAdviser.java
+++ car-department/src/main/java/com/griddynamics/cd/exception/ExceptionAdviser.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -43,6 +45,16 @@ public class ExceptionAdviser extends ResponseEntityExceptionHandler {
         String[] msg = manve.getMessage().split("\\s+");
 
         return buildErrorResponse(msg[0] + ' ' + msg[1], HttpStatus.BAD_REQUEST, LocalDateTime.now(), validationErrors);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException hmnre, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("Failed to parse json");
+
+        String[] msg = Objects.requireNonNull(hmnre.getMessage()).split("\\s+");
+
+        return buildErrorResponse(msg[0] + ' ' + msg[1] + ' ' + msg[2], HttpStatus.BAD_REQUEST, LocalDateTime.now(), null);
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
