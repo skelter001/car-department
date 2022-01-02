af61988892636f88bad61bdf4c2ebacff785ec56
Gamzat <unichtojenmusor@gmail.com>
2022-01-02 15:14:21 +0300

    chore: Remove liquibase xml changesets

---
+++ car-department/src/test/java/com/griddynamics/cd/controller/unit/CarControllerTest.java
@@ -0,0 +1,10 @@
+package com.griddynamics.cd.controller.unit;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class CarControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ car-department/src/test/java/com/griddynamics/cd/service/unit/CarServiceTest.java
@@ -0,0 +1,10 @@
+package com.griddynamics.cd.service.unit;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class CarServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
