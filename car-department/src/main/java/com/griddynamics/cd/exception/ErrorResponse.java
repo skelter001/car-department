45d8f405039b1ad2abe806afa740feba7bb6fe4e
Gamzat <unichtojenmusor@gmail.com>
2022-01-03 03:12:46 +0300

    chore: Optimize imports

--- car-department/src/main/java/com/griddynamics/cd/exception/ErrorResponse.java
+++ car-department/src/main/java/com/griddynamics/cd/exception/ErrorResponse.java
@@ -1,7 +1,10 @@
 package com.griddynamics.cd.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
