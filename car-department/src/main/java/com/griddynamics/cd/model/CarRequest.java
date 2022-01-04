775a340c5df0d099bfced5a2b1fa13e56b380ccf
Gamzat <unichtojenmusor@gmail.com>
2022-01-04 18:21:43 +0300

    fix: Edit vin number regex

--- car-department/src/main/java/com/griddynamics/cd/model/CarRequest.java
+++ car-department/src/main/java/com/griddynamics/cd/model/CarRequest.java
@@ -15,7 +15,7 @@ public class CarRequest {
     private String manufacturer;
     @Pattern(regexp = "[a-zA-Z0-9\s]+", message = "Must contain only letters, numbers or spaces")
     private String model;
-    @Pattern(regexp = "(\\d[A-Z]{4}\\d[A-Z]{2}\\d[A-Z]{3}\\d{5})", message = "Invalid vin number")
+    @Pattern(regexp = "([A-Za-z0-9]{17})", message = "Invalid vin number")
     private String vinNumber;
     @NotNull
     private ColorRequest color;
