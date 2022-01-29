7afc0ce2778b6e7d17bdd44a3e285d0ea1f80f11
Gamzat <unichtojenmusor@gmail.com>
2022-01-29 20:22:06 +0300

    chore: Add no args constructors to models

--- car-department/src/main/java/com/griddynamics/cd/model/Employee.java
+++ car-department/src/main/java/com/griddynamics/cd/model/Employee.java
@@ -3,11 +3,13 @@ package com.griddynamics.cd.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Employee {
 
