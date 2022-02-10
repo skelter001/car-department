437089fdc36e74cdb466e08060ac26ba2638bb06
Gamzat <unichtojenmusor@gmail.com>
2022-02-10 19:27:22 +0300

    fix: Correct query for filtering and sorting in DepartmentRepository

--- car-department/src/main/java/com/griddynamics/cd/repository/DepartmentRepository.java
+++ car-department/src/main/java/com/griddynamics/cd/repository/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.griddynamics.cd.repository;
 
 import com.griddynamics.cd.entity.DepartmentEntity;
 import com.griddynamics.cd.model.DepartmentType;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,13 +15,19 @@ public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Lo
 
     boolean existsByEmailAndIdIsNot(String email, Long id);
 
-    @Query(value = "SELECT d FROM DepartmentEntity as d WHERE " +
-            "(:name is null or d.name like %:name% )" +
-            "and " + "(:email is null or d.email like %:email%) " +
-            "and " + "(:description is null or d.description like %:description%) " +
-            "and " + "(:departmentType is null or d.departmentType=:departmentType)")
-    List<DepartmentEntity> findAllByFilterParams(@Param(value = "name") String name,
-                                                 @Param(value = "email") String email,
-                                                 @Param(value = "description") String description,
-                                                 @Param(value = "departmentType") DepartmentType departmentType);
+    @Query(value =
+            "SELECT * " +
+            "FROM department AS d " +
+            "WHERE " +
+            "(:name IS NULL OR d.name = cast(:name AS TEXT)) " +
+            "AND " + "(:email IS NULL OR d.email = cast(:email AS TEXT)) " +
+            "AND " + "(:description IS NULL OR d.description = cast(:description AS TEXT)) " +
+            "AND " + "(:departmentType IS NULL OR d.department_Type = cast(:#{#departmentType?.name()} AS TEXT)) " +
+            " /*#sort*/",
+            nativeQuery = true)
+    List<DepartmentEntity> findAllByFilterParamsAndSort(@Param(value = "name") String name,
+                                                        @Param(value = "email") String email,
+                                                        @Param(value = "description") String description,
+                                                        @Param(value = "departmentType") DepartmentType departmentType,
+                                                        Sort sort);
 }
